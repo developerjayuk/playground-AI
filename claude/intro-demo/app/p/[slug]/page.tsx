4fe2a7806c7995184a6db0f07ee420a212b5ff4e
Jason James <jason.james@cba-japan.com>
2026-07-27 11:59:58 +0100

    initial changes

---
+++ claude/intro-demo/app/p/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default function PublicNotePage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-semibold">Public note</h1>
+    </div>
+  );
+}
